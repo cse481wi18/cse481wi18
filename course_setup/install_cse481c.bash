0ec92bf35e7344c47022da288065fa3c24651fbf
Justin Huang <jstn@cs.washington.edu>
2018-01-23 18:30:48 -0800

    Added Caddy to course setup script.

--- course_setup/install_cse481c.bash
+++ course_setup/install_cse481c.bash
@@ -27,3 +27,6 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install node
 npm install -g polymer-cli bower
+
+# Caddy
+curl https://getcaddy.com | bash -s personal http.cors
