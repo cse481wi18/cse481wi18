f6ad579e40b41d2fe227f57af56e4e6741c02988
Justin Huang <jstn@cs.washington.edu>
2018-03-01 19:53:29 -0800

    Bail out if sound play is not detected after 10 seconds.

--- fetch_api/src/fetch_api/sound.py
+++ fetch_api/src/fetch_api/sound.py
@@ -9,13 +9,22 @@ VOICE = 'voice_kal_diphone'
 class RobotSound(object):
     def __init__(self):
         self._soundhandle = SoundClient(blocking=True)
+        count = 0
+        self._initialized = False
         while not rospy.is_shutdown() and not self._soundhandle.actionclient.wait_for_server(rospy.Duration(1.0)):
             rospy.logwarn('Waiting for sound play node...')
+            count += 1
+            if count > 10:
+                rospy.logerr('Could not connect to sound play node!')
+                return
+        self._initialized = True
         rospy.loginfo('Sound play ready')
 
     def say(self, text):
-        self._soundhandle.say(text, VOICE)
+        if self._initialized:
+            self._soundhandle.say(text, VOICE)
 
     def play_sound(self, sound_file):
-        self._soundhandle.playWaveFromPkg('fetch_api', 'sounds/{}'.format(sound_file))
+        if self._initialized:
+            self._soundhandle.playWaveFromPkg('fetch_api', 'sounds/{}'.format(sound_file))
 
